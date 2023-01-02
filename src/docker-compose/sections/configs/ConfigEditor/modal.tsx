6fbb984f09486b82c1b798b57c0d21c559879796
Jivaros <jivarosgbeto@gmail.com>
2023-01-03 00:12:02 +0100

    feat: add secret bound

--- src/docker-compose/sections/configs/ConfigEditor/modal.tsx
+++ src/docker-compose/sections/configs/ConfigEditor/modal.tsx
@@ -1,126 +1,126 @@
 import {
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  Heading,
-  ModalCloseButton,
-  ModalProps,
-  VStack,
-} from "@chakra-ui/react";
-import { useCallback } from "react";
-import { useImmer } from "use-immer";
-import { ConfigEditor } from ".";
-import { PrimaryButton } from "../../../../components/Buttons/Primary";
-import { useDockerComposeProject } from "../../../providers/DockerComposeProvider";
-import { ConfigFile, VolumeConfig } from "../../../types";
+ Drawer,
+ DrawerBody,
+ DrawerCloseButton,
+ DrawerContent,
+ DrawerFooter,
+ DrawerHeader,
+ DrawerOverlay,
+ Heading,
+ ModalCloseButton,
+ ModalProps,
+ VStack
+} from '@chakra-ui/react'
+import { useCallback } from 'react'
+import { useImmer } from 'use-immer'
+import { ConfigEditor } from '.'
+import { PrimaryButton } from '../../../../components/Buttons/Primary'
+import { useDockerComposeProject } from '../../../providers/DockerComposeProvider'
+import { ConfigFile } from '../../../types'
 
-export function CreateConfigModal(props: Omit<ModalProps, "children">) {
-  const [content, setContent] = useImmer<{ item: ConfigFile }>({
-    item: {
-      label: "",
-    },
-  });
+export function CreateConfigModal(props: Omit<ModalProps, 'children'>) {
+ const [content, setContent] = useImmer<{ item: ConfigFile }>({
+  item: {
+   label: ''
+  }
+ })
 
-  const { addConfigFile } = useDockerComposeProject();
+ const { addConfigFile } = useDockerComposeProject()
 
-  const onSubmit = useCallback(() => {
-    addConfigFile(content.item);
-    if (props.onClose) {
-      props.onClose();
-    }
-  }, [content, props.onClose]);
+ const onSubmit = useCallback(() => {
+  addConfigFile(content.item)
+  if (props.onClose) {
+   props.onClose()
+  }
+ }, [content, props.onClose])
 
-  return (
-    <Drawer
-      size="xl"
-      isCentered
-      placement="left"
-      closeOnOverlayClick={false}
-      closeOnEsc={false}
-      scrollBehavior="inside"
-      {...props}
-    >
-      <DrawerOverlay />
-      <DrawerContent bg="surface">
-        <DrawerHeader>
-          <Heading fontSize="lg">Add a config</Heading>
-          <ModalCloseButton />
-        </DrawerHeader>
-        <DrawerBody>
-          <VStack alignItems="flex-start" spacing={4}>
-            <ConfigEditor
-              value={content.item}
-              onChange={(update) => {
-                setContent((draft) => {
-                  draft.item = update;
-                });
-              }}
-            />
-          </VStack>
-        </DrawerBody>
-        <DrawerFooter>
-          <PrimaryButton onClick={onSubmit}>Add this config file</PrimaryButton>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  );
+ return (
+  <Drawer
+   size='xl'
+   isCentered
+   placement='left'
+   closeOnOverlayClick={false}
+   closeOnEsc={false}
+   scrollBehavior='inside'
+   {...props}
+  >
+   <DrawerOverlay />
+   <DrawerContent bg='surface'>
+    <DrawerHeader>
+     <Heading fontSize='lg'>Add a config</Heading>
+     <ModalCloseButton />
+    </DrawerHeader>
+    <DrawerBody>
+     <VStack alignItems='flex-start' spacing={4}>
+      <ConfigEditor
+       value={content.item}
+       onChange={(update) => {
+        setContent((draft) => {
+         draft.item = update
+        })
+       }}
+      />
+     </VStack>
+    </DrawerBody>
+    <DrawerFooter>
+     <PrimaryButton onClick={onSubmit}>Add this config file</PrimaryButton>
+    </DrawerFooter>
+   </DrawerContent>
+  </Drawer>
+ )
 }
 
 export function UpdateConfigModal(
-  props: Omit<ModalProps, "children"> & { value: ConfigFile }
+ props: Omit<ModalProps, 'children'> & { value: ConfigFile }
 ) {
-  const [content, setContent] = useImmer<{ item: ConfigFile }>({
-    item: {
-      ...props.value,
-    },
-  });
+ const [content, setContent] = useImmer<{ item: ConfigFile }>({
+  item: {
+   ...props.value
+  }
+ })
 
-  const { addConfigFile } = useDockerComposeProject();
+ const { addConfigFile } = useDockerComposeProject()
 
-  const onSubmit = useCallback(() => {
-    addConfigFile(content.item);
-    if (props.onClose) {
-      props.onClose();
-    }
-  }, [content, props.onClose]);
+ const onSubmit = useCallback(() => {
+  addConfigFile(content.item)
+  if (props.onClose) {
+   props.onClose()
+  }
+ }, [content, props.onClose])
 
-  return (
-    <Drawer
-      size="xl"
-      isFullHeight
-      placement="right"
-      isCentered
-      closeOnOverlayClick={false}
-      closeOnEsc={false}
-      scrollBehavior="inside"
-      {...props}
-    >
-      <DrawerOverlay />
-      <DrawerContent bg="surface">
-        <DrawerHeader>
-          <Heading fontSize="lg">Update config {props.value.label}</Heading>
-          <DrawerCloseButton />
-        </DrawerHeader>
-        <DrawerBody>
-          <VStack alignItems="flex-start" spacing={4}>
-            <ConfigEditor
-              value={content.item}
-              onChange={(update) => {
-                setContent((draft) => {
-                  draft.item = update;
-                });
-              }}
-            />
-          </VStack>
-        </DrawerBody>
-        <DrawerFooter>
-          <PrimaryButton onClick={onSubmit}>Apply changes</PrimaryButton>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  );
+ return (
+  <Drawer
+   size='xl'
+   isFullHeight
+   placement='right'
+   isCentered
+   closeOnOverlayClick={false}
+   closeOnEsc={false}
+   scrollBehavior='inside'
+   {...props}
+  >
+   <DrawerOverlay />
+   <DrawerContent bg='surface'>
+    <DrawerHeader>
+     <Heading fontSize='lg'>Update config {props.value.label}</Heading>
+     <DrawerCloseButton />
+    </DrawerHeader>
+    <DrawerBody>
+     <VStack alignItems='flex-start' spacing={4}>
+      <ConfigEditor
+       value={content.item}
+       onChange={(update) => {
+        setContent((draft) => {
+         draft.item = update
+        })
+       }}
+      />
+     </VStack>
+    </DrawerBody>
+    <DrawerFooter>
+     <PrimaryButton onClick={onSubmit}>Apply changes</PrimaryButton>
+    </DrawerFooter>
+   </DrawerContent>
+  </Drawer>
+ )
 }
