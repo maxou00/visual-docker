55a2c65042af4405572965030356e1f06f08a453
Jivaros <jivarosgbeto@gmail.com>
2023-01-03 00:19:50 +0100

    refactor: refact BoundSecret component

---
+++ src/docker-compose/sections/services/ServiceEditor/BoundSecretEditor/FieldEntry.tsx
@@ -0,0 +1,109 @@
+import {
+ Button,
+ FormControl,
+ FormHelperText,
+ FormLabel,
+ HStack,
+ Input,
+ Select,
+ VStack
+} from '@chakra-ui/react'
+import { useDockerComposeProject } from '../../../../providers/DockerComposeProvider'
+import { BindConfigToService } from '../../../../types'
+
+export default function FieldEntry({
+ entry,
+ onChange,
+ onDelete
+}: {
+ entry: BindConfigToService
+ onChange: (entry: BindConfigToService) => any
+ onDelete: () => any
+}) {
+ const composer = useDockerComposeProject()
+
+ return (
+  <VStack w='full' alignItems='center' spacing={2}>
+   <Select
+    size='sm'
+    value={entry.source}
+    onChange={(nodeEv) => {
+     onChange({ ...entry, source: nodeEv.target.value })
+    }}
+   >
+    <option value=''>Choose a Secret</option>
+    {composer.state.secrets.map((secret) => {
+     return (
+      <option key={secret.label} value={secret.label}>
+       {secret.label}
+      </option>
+     )
+    })}
+   </Select>
+   <HStack w='full' alignItems='flex-start' spacing={4}>
+    <FormControl>
+     <FormLabel>Target path within the container</FormLabel>
+     <Input
+      size='sm'
+      flexGrow={1}
+      placeholder=''
+      value={entry.target}
+      onChange={({ target }) => onChange({ ...entry, target: target.value })}
+     />
+     <FormHelperText>
+      Enter the path where this config should be mounted within the container.
+      Default to {'/<config-name>'}
+     </FormHelperText>
+    </FormControl>
+    <FormControl>
+     <FormLabel>Permissions assigned the config file</FormLabel>
+     <Input
+      size='sm'
+      flexGrow={1}
+      placeholder=''
+      value={entry.mode}
+      onChange={({ target }) => onChange({ ...entry, target: target.value })}
+     />
+     <FormHelperText>
+      Linux permission mode assigned to the config file. Like 0444 or 777
+     </FormHelperText>
+    </FormControl>
+   </HStack>
+   <HStack w='full' spacing={4}>
+    <FormControl>
+     <FormLabel>UID (user id) that owns the config file</FormLabel>
+     <Input
+      size='sm'
+      flexGrow={1}
+      placeholder=''
+      value={entry.uid}
+      onChange={({ target }) => onChange({ ...entry, target: target.value })}
+     />
+     <FormHelperText>
+      Default to the uid of the USER running the container. It's a numeric
+      value.
+     </FormHelperText>
+    </FormControl>
+    <FormControl>
+     <FormLabel>GID (group id) that owns the config file</FormLabel>
+     <Input
+      size='sm'
+      flexGrow={1}
+      placeholder=''
+      value={entry.gid}
+      onChange={({ target }) => onChange({ ...entry, target: target.value })}
+     />
+     <FormHelperText>
+      Default to the gid of the USER running the container. It's a numeric
+      value.
+     </FormHelperText>
+    </FormControl>
+   </HStack>
+   <HStack w='full' justifyContent='flex-end'>
+    <Button size='sm' onClick={() => onDelete()} aria-label={''}>
+     Unbind this config
+    </Button>
+   </HStack>
+  </VStack>
+ )
+}
--- src/docker-compose/sections/services/ServiceEditor/BoundSecretEditor/index.tsx
+++ src/docker-compose/sections/services/ServiceEditor/BoundSecretEditor/index.tsx
@@ -1,121 +1,10 @@
-import {
- Button,
- FormControl,
- FormHelperText,
- FormLabel,
- HStack,
- Input,
- Select,
- Text,
- VStack
-} from '@chakra-ui/react'
+import { HStack, Text, VStack } from '@chakra-ui/react'
 import { nanoid } from 'nanoid'
 import { useCallback, useState } from 'react'
 import { toast } from 'react-hot-toast'
 import { PrimaryButton } from '../../../../../components/Buttons/Primary'
-import { useDockerComposeProject } from '../../../../providers/DockerComposeProvider'
 import { BindConfigToService } from '../../../../types'
-
-type Entry = BindConfigToService
-
-const initial: Entry = { id: nanoid(), source: '' }
-
-const FieldEntry = ({
- entry,
- onChange,
- onDelete
-}: {
- entry: Entry
- onChange: (entry: Entry) => any
- onDelete: () => any
-}) => {
- const composer = useDockerComposeProject()
-
- return (
-  <VStack w='full' alignItems='center' spacing={2}>
-   <Select
-    size='sm'
-    value={entry.source}
-    onChange={(nodeEv) => {
-     onChange({ ...entry, source: nodeEv.target.value })
-    }}
-   >
-    <option value=''>Choose a Secret</option>
-    {composer.state.secrets.map((secret) => {
-     return (
-      <option key={secret.label} value={secret.label}>
-       {secret.label}
-      </option>
-     )
-    })}
-   </Select>
-   <HStack w='full' alignItems='flex-start' spacing={4}>
-    <FormControl>
-     <FormLabel>Target path within the container</FormLabel>
-     <Input
-      size='sm'
-      flexGrow={1}
-      placeholder=''
-      value={entry.target}
-      onChange={({ target }) => onChange({ ...entry, target: target.value })}
-     />
-     <FormHelperText>
-      Enter the path where this config should be mounted within the container.
-      Default to {'/<config-name>'}
-     </FormHelperText>
-    </FormControl>
-    <FormControl>
-     <FormLabel>Permissions assigned the config file</FormLabel>
-     <Input
-      size='sm'
-      flexGrow={1}
-      placeholder=''
-      value={entry.mode}
-      onChange={({ target }) => onChange({ ...entry, target: target.value })}
-     />
-     <FormHelperText>
-      Linux permission mode assigned to the config file. Like 0444 or 777
-     </FormHelperText>
-    </FormControl>
-   </HStack>
-   <HStack w='full' spacing={4}>
-    <FormControl>
-     <FormLabel>UID (user id) that owns the config file</FormLabel>
-     <Input
-      size='sm'
-      flexGrow={1}
-      placeholder=''
-      value={entry.uid}
-      onChange={({ target }) => onChange({ ...entry, target: target.value })}
-     />
-     <FormHelperText>
-      Default to the uid of the USER running the container. It's a numeric
-      value.
-     </FormHelperText>
-    </FormControl>
-    <FormControl>
-     <FormLabel>GID (group id) that owns the config file</FormLabel>
-     <Input
-      size='sm'
-      flexGrow={1}
-      placeholder=''
-      value={entry.gid}
-      onChange={({ target }) => onChange({ ...entry, target: target.value })}
-     />
-     <FormHelperText>
-      Default to the gid of the USER running the container. It's a numeric
-      value.
-     </FormHelperText>
-    </FormControl>
-   </HStack>
-   <HStack w='full' justifyContent='flex-end'>
-    <Button size='sm' onClick={() => onDelete()} aria-label={''}>
-     Unbind this config
-    </Button>
-   </HStack>
-  </VStack>
- )
-}
+import FieldEntry from './FieldEntry'
 
 export default function BoundSecretConfigEditor({
  value,
@@ -124,7 +13,7 @@ export default function BoundSecretConfigEditor({
  value: any
  onChange: (v: any) => any
 }) {
- const [keys, setKeys] = useState<Entry[]>([])
+ const [keys, setKeys] = useState<BindConfigToService[]>([])
 
  const onAppendKey = useCallback(() => {
   let cpy = [...keys]
@@ -139,7 +28,7 @@ export default function BoundSecretConfigEditor({
  }, [keys, onChange])
 
  const onEntryChange = useCallback(
-  (id: string, entry: Entry) => {
+  (id: string, entry: BindConfigToService) => {
    let cpy = [...keys]
    let index = cpy.findIndex((k) => k.id === id)
    if (index > -1) {
